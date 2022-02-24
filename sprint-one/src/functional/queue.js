08e015233f7025f86c7593200cb8d66a6fde17f6
ivyhu <ajianouc@gmail.com>
2022-02-24 13:18:13 -0800

    completed functional instantiation

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,28 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var frontnum = 0;
+  var key = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[key] = value;
+    key++;
   };
 
   someInstance.dequeue = function() {
+    let size = someInstance.size();
+    if (size > 0) {
+      let temp = storage[frontnum];
+      delete storage[frontnum];
+      frontnum ++;
+      return temp;
+    }
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,13 +6,23 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    let size = someInstance.size();
+    storage[size + 1] = value;
   };
 
   someInstance.pop = function() {
+    let size = someInstance.size();
+    if (size > 0) {
+      let temp = storage[size];
+      delete storage[size];
+      return temp;
+    }
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
 };
+
