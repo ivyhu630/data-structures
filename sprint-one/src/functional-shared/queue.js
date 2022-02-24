c6ac11b15a08d2ebad5874664b20bf3e8f4ac601
ivyhu <ajianouc@gmail.com>
2022-02-24 15:10:00 -0800

    completed functional-shared & propotypal

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+  newQueue.storage = {};
+  newQueue.firstIndex = 0;
+  newQueue.key = 0;
+
+  _.extend(newQueue, queueMethods);
+
+  return newQueue;
 };
 
 var queueMethods = {};
 
+
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.key] = value;
+  this.key++;
+};
+
+queueMethods.dequeue = function() {
+  if (this.size() > 0) {
+    let temp = this.storage[this.firstIndex];
+    delete this.storage[this.firstIndex];
+    this.firstIndex++;
+    return temp;
+  }
+};
 
--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.storage = {};
+  newQueue.firstIndex = 0;
+  newQueue.key = 0;
+  return newQueue;
 };
 
 var queueMethods = {};
 
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.key] = value;
+  this.key++;
+};
+
+queueMethods.dequeue = function() {
+  if (this.size() > 0) {
+    let temp = this.storage[this.firstIndex];
+    delete this.storage[this.firstIndex];
+    this.firstIndex++;
+    return temp;
+  }
+};
 
